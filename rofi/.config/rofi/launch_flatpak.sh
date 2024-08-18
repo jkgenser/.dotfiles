d2ee87da52bb82c6d6d96a00788ffd931491524d
Jerry Genser <jerrygenser@gmail.com>
2024-08-18 16:23:54 -0400

    fix: rofi launcher is case insensitive

--- rofi/.config/rofi/launch_flatpak.sh
+++ rofi/.config/rofi/launch_flatpak.sh
@@ -4,7 +4,7 @@
 apps=$(flatpak list --app --columns=application,name | awk '{print $2 ":" $1}')
 
 # Display the list of apps and capture the selected app
-selected_app=$(echo "$apps" | rofi -dmenu -p "Launch Flatpak App:" | cut -d ":" -f 2)
+selected_app=$(echo "$apps" | rofi -dmenu -p -i "Launch Flatpak App:" | cut -d ":" -f 2)
 
 # If an app is selected, launch it
 if [ -n "$selected_app" ]; then
