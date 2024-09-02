e3a31d29f6d5dcbada648aff19bfe64be497bb97
Jerry Genser <jerrygenser@gmail.com>
2024-09-02 10:29:47 -0400

    add: get dotfiles working

--- bin/.local/scripts/start-nvim-container.sh
+++ bin/.local/scripts/start-nvim-container.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-NVIM_CONFIG_NAME=nvim-lazy
+NVIM_APPNAME=nvim-lazy
 IMAGE_NAME=neovim-dev
 
 podman run --rm --name neovim-container --replace -it \
-  -v $HOME/.config/$NVIM_CONFIG_NAME:/root/.config/nvim:Z \
-  -v $(pwd):/workspace:Z \
+  -v $HOME/.dotfiles/nvim/.config/$NVIM_APPNAME:/root/.config/nvim:z \
+  -v $(pwd):/workspace:z \
   -w /workspace \
-  $IMAGE_NAME /bin/bash -c "$@"
+  $IMAGE_NAME /bin/bash -c "nvim $@"
