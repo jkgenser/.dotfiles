695c4095c821d410f020125e4426e0b67fe05e26
Jerry Genser <jerrygenser@gmail.com>
2024-09-03 10:52:23 -0400

    allow multiple nvims

--- bin/.local/scripts/start-nvim-container.sh
+++ bin/.local/scripts/start-nvim-container.sh
@@ -2,8 +2,9 @@
 
 NVIM_APPNAME=nvim-lazy
 IMAGE_NAME=neovim-dev
+CONTAINER_NAME="neovim-container-$(date +%s)"
 
-podman run --rm --name neovim-container --replace -it \
+podman run --rm --name ${CONTAINER_NAME} -it \
   -v $HOME/.dotfiles/nvim/.config/$NVIM_APPNAME:/root/.config/nvim:z \
   -v $(pwd):/workspace:z \
   -w /workspace \
