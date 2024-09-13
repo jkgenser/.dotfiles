ad45bb30f4974b6c9181ee604a2eb1f246abb1eb
Jerry Genser <jerrygenser@gmail.com>
2024-09-13 12:41:41 -0400

    add: turn off linenumbers

--- nvim/.config/nvim-lazy/lua/config/keymaps.lua
+++ nvim/.config/nvim-lazy/lua/config/keymaps.lua
@@ -5,4 +5,3 @@
 vim.keymap.set("n", "<Esc>", "<cmd>noh<CR>", { desc = "general clear highlights" })
 vim.keymap.set("n", "<leader>/", "gcc", { desc = "comment toggle", remap = true })
 vim.keymap.set("v", "<leader>/", "gc", { desc = "comment toggle", remap = true })
--- vim.g.clipboard = "unnamedplus"
